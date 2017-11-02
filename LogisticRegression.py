b9fddfde70fe0b932a231fc1af242a9b7753fcf2
kushal <kushaldeveloper@gmail.com>
2017-11-02 17:58:15 -0400

    learn

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,66 +1,35 @@
-from numpy import loadtxt, where, zeros, e, array, log, ones, append, linspace
-from pylab import scatter, show, legend, xlabel, ylabel, contour, title
-from scipy.optimize import fmin_bfgs
-
+import numpy
 
 def sigmoid(X):
     '''Compute the sigmoid function '''
-    #d = zeros(shape=(X.shape))
-
-    den = 1.0 + e ** (-1.0 * X)
-
+    # d = zeros(shape=(X.shape))
+    den = 1.0 + numpy.e ** (-1.0 * X)
     d = 1.0 / den
-
     return d
 
 
-def cost_function_reg(theta, X, y, l):
-    '''Compute the cost and partial derivatives as grads
-    '''
+def compute_cost(theta, X, y):  # computes cost given predicted and actual values
+    m = X.shape[0]  # number of training examples
+    theta = numpy.reshape(theta, (len(theta), 1))
+    # y = reshape(y,(len(y),1))
+    J = (1. / m) * (
+        - numpy.transpose(y).dot(numpy.log(sigmoid(X.dot(theta)))) - numpy.transpose(1 - y).dot(
+            numpy.log(1 - sigmoid(X.dot(theta)))))
+    grad = numpy.transpose((1. / m) * numpy.transpose(sigmoid(X.dot(theta)) - y).dot(X))
+    # optimize.fmin expects a single value, so cannot return grad
+    return J[0][0]  # ,grad
 
-    h = sigmoid(X.dot(theta))
 
-    thetaR = theta[1:, 0]
-
-    J = (1.0 / m) * ((-y.T.dot(log(h))) - ((1 - y.T).dot(log(1.0 - h)))) \
-            + (l / (2.0 * m)) * (thetaR.T.dot(thetaR))
-
+def compute_grad(theta, X, y):
+    # print theta.shape
+    theta.shape = (1, 3)
+    grad = numpy.zeros(3)
+    h = sigmoid(X.dot(theta.T))
     delta = h - y
-    sumdelta = delta.T.dot(X[:, 1])
-    grad1 = (1.0 / m) * sumdelta
-
-    XR = X[:, 1:X.shape[1]]
-    sumdelta = delta.T.dot(XR)
-
-    grad = (1.0 / m) * (sumdelta + l * thetaR)
-
-    out = zeros(shape=(grad.shape[0], grad.shape[1] + 1))
-
-    out[:, 0] = grad1
-    out[:, 1:] = grad
-
-    return J.flatten(), out.T.flatten()
-
-
-
-
-m, n = X.shape
-
-y.shape = (m, 1)
-
-it = map_feature(X[:, 0], X[:, 1])
-
-#Initialize theta parameters
-initial_theta = zeros(shape=(it.shape[1], 1))
-
-#Set regularization parameter lambda to 1
-l = 1
-
-# Compute and display initial cost and gradient for regularized logistic
-# regression
-cost, grad = cost_function_reg(initial_theta, it, y, l)
-
-def decorated_cost(theta):
-    return cost_function_reg(theta, it, y, l)
-
-print fmin_bfgs(decorated_cost, initial_theta, maxfun=400)
+    l = grad.size
+    for i in range(l):
+        sumdelta = delta.T.dot(X[:, i])
+        grad[i] = (1.0 / h) * sumdelta * - 1
+    theta.shape = (3,)
+    print(grad)
+    return grad
