ea9ed8d6da2476444af8589f28232a5785c9fd91
Kushal Hada <kushaldeveloper@gmail.com>
2017-11-04 17:30:42 -0400

    add my threshold as a variable

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -199,7 +199,8 @@ def main():
         w_opt = my_train_binary(x_train, y_train)
         draw_result_binary(x_train, x_test, y_train, y_test, w_opt)
         a = b = c = 0.0
-        while get_global_score() < 0.7:
+        my_threshold = 0.50
+        while get_global_score() < my_threshold:
             a = random.random()
             b = random.random()
             c = random.random()
